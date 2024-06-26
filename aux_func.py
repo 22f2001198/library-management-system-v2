e07c03c63216cb7d3842c62cf544aa078d731a07
Mohit Kumar <22f2001198@ds.study.iitm.ac.in>
2024-06-26 22:07:38 +0530

    committed

---
+++ aux_func.py
@@ -0,0 +1,13 @@
+from flask import jsonify
+from functools import wraps
+from flask_jwt_extended import get_jwt_identity
+from models import db,User
+
+def is_admin(func):
+    @wraps(func)
+    def admin_auth(*args,**kwargs):
+        current=get_jwt_identity()
+        if current['role']=='admin':
+            return func(*args,**kwargs)
+        return jsonify({'msg':'Access Denied'})
+    return admin_auth
--- init.py
+++ init.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
-from config import secret,project_db
+from config import secret,project_db,jwt_secret_key
 from os import path
 
 db=SQLAlchemy()          #db initialisation
@@ -10,14 +10,15 @@ def create_app():
     app=Flask(__name__)  #app creation
     app.config['SECRET_KEY']=secret                  #app config
     app.config['SQLALCHEMY_DATABASE_URI']=project_db
+    app.config['JWT_SECRET_KEY']=jwt_secret_key
     db.init_app(app)
     jwt=JWTManager(app)    #jwt initialisation
 
     from admin_routes import admin        #import components
     from user_routes import user
 
-    app.register_blueprint(admin)        #register blueprints
-    app.register_blueprint(user)
+    app.register_blueprint(admin,url_prefix='/')        #register blueprints
+    app.register_blueprint(user,url_prefix='/')
 
     from models import User              #get db models
     create_database(app)
